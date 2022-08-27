9a40b0b16c520ec7b283f1476705e8f1c4b66717
gitpacho <crepachonpa@unal.edu.co>
2022-08-27 13:33:37 -0500

    Ejercicios  en clase

---
+++ EJERCICIOS/1_HolaMundo.py
@@ -0,0 +1,10 @@
+# Estamos utilizando la funcion print para ejecutar la linea de codigo
+# más familiar en todos los lenguajes
+
+print("Hola mundo")
+print(1)
+print(True)
+print(12.54345354)
+print([1,2,3])
+print((1,2,3))
+print("variable = ", 12, "Este numero es importante", sep=" ### ")
