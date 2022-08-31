83940152a1a11bac52768bfb70313d5462f93046
gitpacho <crepachonpa@unal.edu.co>
2022-08-31 14:27:59 -0500

    operadores

--- EJERCICIOS/EJERCICIOS_2.py
+++ EJERCICIOS/EJERCICIOS_2.py
@@ -1,6 +1,6 @@
 """
 En este archivo encontramos ejercicios con los siguientes temas: 
-funciones integradas de python, uso de condicionales y ciclo while
+funciones integradas de python, uso de condicionales y ciclos while y for
 """
 
 #====================== EJERCICIOS FUNCIONES INTEGRADAS ====================
@@ -156,3 +156,125 @@ El programa termina cuando se ingresa el número cero."""
 => 2,4,5,8,10,11,14,16,17,20 ...598, 599
 => 2,4,8,16,32,64,128, .. 1048576
 => 1,1,2,3,5,8, ... 2178309"""
+
+
+
+#====================== EJERCICIOS CICLO FOR ====================
+
+#==> EJERCICIO 1 
+""" Utilice el ciclo for para recorrer los siguientes iterables, luego imprimalos:
+     a) Posiciones = range(10) ,      
+     b) Edades = range(10,30,2)
+     c) Tamaños = range(1,11, 3)
+     d) letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+     e) Estudiantes = ["Cristian", "José", "María", "Juanita", "Marly"]
+     f) Calificaciones = (5, 5, 5, 2, 4) """
+
+#==> EJERCICIO 2 
+""" Imprimir los siguientes numeros (utilice el iterable que más le convenga):
+     a) 2, 3, 4, 5, 6, 7, 8, 9, 10
+     b) 3, 6, 9, 12, ... 300
+     c) 5, -5, 20, 25, 100, 0, -1, 21, 32, 57, 26
+     d) 1, -1, 1, -1, 1, -1, 1, 
+     e) "Z", "Y", "X", "W" , ... "B", "A"
+     f) "A", 1, "B", 2, "C", 3 """
+
+#==> EJERCICIO 3
+""" Realice los siguientes programas:
+      a) Programa que calcule los primeros 100 terminos de la serie de fibonacci
+      b) Programa que determine todos los divisores de un numero n
+      c) Programa que determine si un número n es primo
+      d) Programa que sume los digitos de un numero cualquiera. Ejemplo: numero=> 8791, rta=> 25 
+      e) Programa que sume los digitos pares de un numero cualquiera """
+
+#==> EJERCICIO 4 
+""" El rendimiento de los empleados de una empresa es el siguiente:
+
+--------------  Emplea_1  Emplea_2  Emplea_3  Emplea_4  Emplea_5  Emplea_6  Emplea_7  Emplea_8  Emplea_9  Emplea_10  Emplea_11  Emplea_12  Emplea_13  Emplea_14  Emplea_15  Emplea_16  Emplea_17  Emplea_18  Emplea_19  Emplea_20  Emplea_21  Emplea_22  Emplea_23  Emplea_24  Emplea_25  Emplea_26  Emplea_27 
+Rendimiento --    "C"       "B"      "B"        "B"        "C"      "B"      "A"        "C"       "B"        "A"        "C"       "B"        "B"        "B"         "B"        "A"       "B"        "A"         "A"        "C"       "B"        "B"        "B"         "B"         "C"       "A" 
+
+Donde "A" es alto, "B" aceptable  y "C"  insuficiente. 
+Determine ¿cuales empleados pueden solicitar un aumento salarial y cuáles pueden ser despedidos? """
+
+#==> EJERCICIO 5 
+""" El rendimiento deportivo de un grupo de atletas es el siguiente:
+
+-------------- Deportista_1  Deportista_2  Deportista_3  Deportista_4  Deportista_5  Deportista_6  Deportista_7  Deportista_8  Deportista_9  Deportista_10  Deportista_11  Deportista_12  Deportista_13  Deportista_14  Deportista_15  Deportista_16  Deportista_17  Deportista_18  Deportista_19  Deportista_20  Deportista_21  Deportista_22  Deportista_23  Deportista_24  Deportista_25  Deportista_26  Deportista_27 
+Rendimiento --    "A"           "B"           "C"            "B"            "B"           "B"          "C"           "B"             "A"           "C"          "B"          "A"             "C"             "B"          "B"              "B"           "B"             "A"           "B"            "A"           "A"             "C"             "B"             "B"          "B"            "B"            "C"             
+Edad ---------     42            60            18             20             35            25           60            30              19            42           21           17              39              30           28               34            27              23            23             20            25             "40"             31             32            45             26             17             
+
+Donde "A" es alto, "B" aceptable  y "C"  insuficiente. Determine:
+  => ¿Cuántos atletas tienen un rendimiento alto, aceptable e insuficiente?
+  => ¿Cuántos atletas mayores de 40 años, tienen rendimiento alto?
+  => ¿Cuantos atletas menores de 25, tienen un rendimiento insuficiente?
+  => ¿Cuales deportistas entre 30 y 40 años tienen rendimiento aceptable?
+  => ¿Cuales deportistas menores de 30 tienen rendimiento alto
+  => ¿Cuales deportistas mayores de 35 tienen rendimiento insuficiente?
+(Intente utilizar un solo ciclo para resolver las preguntas) """
+
+#==> EJERCICIO 6 
+""" El costo de las entradas a cine es el siguiente:
+                          ----------- 2D ------------        ---------- 3D -------------      
+                          ----NIÑO----  ----ADULTO---        ---NIÑO----  ----ADULTO----
+ precio (Lunes a viernes)     $5000          $8000              $8000         $12000
+ precio (Sabado, domingo)     $7000          $9000              $9000         $15000
+
+Cada una de las ventas de boletas en una semana, se registraron de la siguiente manera:
+("3D_3NIÑOS_MARTES", "3D_2ADULTOS_MARTES")
+("2D_3NIÑOS_MARTES", "2D_1ADULTOS_MARTES")
+("2D_0NIÑOS_MARTES", "2D_2ADULTOS_MARTES")
+("2D_0NIÑOS_MARTES", "2D_2ADULTOS_MARTES")
+("3D_0NIÑOS_MARTES", "3D_1ADULTOS_MARTES")
+("2D_2NIÑOS_MARTES", "2D_1ADULTOS_MARTES")
+("2D_0NIÑOS_MARTES", "2D_2ADULTOS_MARTES")
+("2D_0NIÑOS_MARTES", "2D_2ADULTOS_MARTES")
+("3D_0NIÑOS_MARTES", "3D_3ADULTOS_MARTES")
+("3D_3NIÑOS_MARTES", "3D_4ADULTOS_MARTES")
+("2D_2NIÑOS_MARTES", "2D_4ADULTOS_MARTES")
+("2D_1NIÑOS_MARTES", "2D_4ADULTOS_MARTES")
+("3D_1NIÑOS_MARTES", "3D_1ADULTOS_MARTES")
+("3D_0NIÑOS_MIERCOLES", "3D_3ADULTOS_MIERCOLES")
+("3D_3NIÑOS_MIERCOLES", "3D_4ADULTOS_MIERCOLES")
+("2D_1NIÑOS_MIERCOLES", "2D_1ADULTOS_MIERCOLES")
+("2D_0NIÑOS_MIERCOLES", "2D_2ADULTOS_MIERCOLES")
+("2D_0NIÑOS_MIERCOLES", "2D_2ADULTOS_MIERCOLES")
+("3D_3NIÑOS_MIERCOLES", "3D_4ADULTOS_MIERCOLES")
+("3D_1NIÑOS_MIERCOLES", "3D_4ADULTOS_MIERCOLES")
+("3D_1NIÑOS_MIERCOLES", "3D_1ADULTOS_MIERCOLES")
+("3D_3NIÑOS_JUEVES", "3D_4ADULTOS_JUEVES")
+("2D_2NIÑOS_JUEVES", "2D_1ADULTOS_JUEVES")
+("2D_0NIÑOS_JUEVES", "2D_2ADULTOS_JUEVES")
+("2D_1NIÑOS_JUEVES", "2D_1ADULTOS_JUEVES")
+("3D_2NIÑOS_JUEVES", "3D_1ADULTOS_JUEVES")
+("3D_0NIÑOS_JUEVES", "3D_3ADULTOS_JUEVES")
+("2D_3NIÑOS_JUEVES", "2D_4ADULTOS_JUEVES")
+("2D_1NIÑOS_JUEVES", "2D_4ADULTOS_JUEVES")
+("2D_2NIÑOS_VIERNES", "2D_1ADULTOS_VIERNES")
+("3D_1NIÑOS_VIERNES", "3D_1ADULTOS_VIERNES")
+("2D_0NIÑOS_VIERNES", "2D_2ADULTOS_VIERNES")
+("2D_0NIÑOS_VIERNES", "2D_2ADULTOS_VIERNES")
+("3D_0NIÑOS_VIERNES", "3D_3ADULTOS_VIERNES")
+("3D_3NIÑOS_VIERNES", "3D_4ADULTOS_VIERNES")
+("2D_2NIÑOS_VIERNES", "2D_4ADULTOS_VIERNES")
+("2D_1NIÑOS_VIERNES", "2D_4ADULTOS_VIERNES")
+("2D_1NIÑOS_SABADO", "2D_1ADULTOS_SABADO")
+("2D_0NIÑOS_SABADO", "2D_2ADULTOS_SABADO")
+("2D_0NIÑOS_SABADO", "2D_2ADULTOS_SABADO")
+("3D_0NIÑOS_SABADO", "3D_3ADULTOS_SABADO")
+("3D_3NIÑOS_SABADO", "3D_4ADULTOS_SABADO")
+("2D_1NIÑOS_SABADO", "2D_4ADULTOS_SABADO")
+("2D_1NIÑOS_SABADO", "2D_4ADULTOS_SABADO")
+("3D_1NIÑOS_SABADO", "3D_1ADULTOS_SABADO")
+("2D_1NIÑOS_DOMINGO", "2D_1ADULTOS_DOMINGO")
+("3D_2NIÑOS_DOMINGO", "3D_4ADULTOS_DOMINGO")
+("3D_0NIÑOS_DOMINGO", "3D_5ADULTOS_DOMINGO")
+("3D_1NIÑOS_DOMINGO", "3D_2ADULTOS_DOMINGO")
+("3D_0NIÑOS_DOMINGO", "3D_3ADULTOS_DOMINGO")
+("2D_3NIÑOS_DOMINGO", "2D_4ADULTOS_DOMINGO")
+("2D_1NIÑOS_DOMINGO", "2D_3ADULTOS_DOMINGO")
+("3D_1NIÑOS_DOMINGO", "3D_2ADULTOS_DOMINGO")
+
+    a) Determine el numero de boletas vendidas para niños, entre semana.
+    b) Determine las ventas (dinero) de adultos que fueron los fines de semana a 3D.
+    c) Determine el numero de boletas vendidas totales.
+    d) Determine las ventas (dinero) totales. """
