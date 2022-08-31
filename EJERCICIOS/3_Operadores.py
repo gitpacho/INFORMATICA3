59629b9120770f52be559bc6fd6c5f54e281219e
gitpacho <crepachonpa@unal.edu.co>
2022-08-31 15:02:52 -0500

    operaciones aritmeticas y logicas

---
+++ EJERCICIOS/3_Operadores.py
@@ -0,0 +1,61 @@
+########### Operador de asignación:
+
+a = 1
+b = 2
+c = 3
+
+########### Operadores aritméticos:
+
+print("suma            ==>", 1+2+3)
+print("mult y suma     ==>", 1*2+3)
+print("suma y mult     ==>", 1+2*3)
+print("division entera ==>", 5//3)
+print("division entera ==>", 0//3)
+print("residuo         ==>", 5%2)
+print("residuo         ==>", 13%9)
+print("potenciacion    ==>", 9**2)
+print("potenciacion    ==>", 9**0.5)
+
+
+ ## orden de operaciones: **,
+ #                        */,
+ #                        +-,
+
+print("concatenacion ==>", "hola" + " mundo")
+print("replicacion   ==>", "a" * 10)
+print("concatenacion ==>", [1] + [1,2,3])
+print("repiclacion   ==>", [0] * 10)
+
+
+######### Operadores lógicos
+
+print("and ==>", True and False) #Solo es verdadero si ambos son verdadero
+print("and ==>", False and True)
+print("or ==>", True or False)  #Solo es falso si ambos son falsos
+print("or ==>", False or False)
+
+#¿Se pueden utilizar solo con booleanos?
+print(1 and 1)
+print(1 and 0)
+print(19 and 20)
+print(0 and 100)
+print(100 or 0)
+print(-2 or 20)
+print("cristan" and "elias")
+print("Unal" and " ")
+
+## para pensar:
+   #Desarrolle un algoritmo, que me imprima si un numero es mayor de 18,
+   #sin utilizar el condicional if, utilice operadores lógicos para ello
+
+
+
+############## Operadores de comparación
+
+
+
+
+
+
+
+
