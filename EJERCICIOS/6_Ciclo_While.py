e91a32b921464749444dfa7537415f87a4b0b907
gitpacho <crepachonpa@unal.edu.co>
2022-09-07 15:50:24 -0500

    ciclo while tema y ejercicios clase

--- EJERCICIOS/6_Ciclo_While.py
+++ EJERCICIOS/6_Ciclo_While.py
@@ -19,4 +19,66 @@ while True:
         print("Vamos a romper la ejecución")
         break
 
+"""
+Imprima los numeros del 20 al 50, utilizando el
+ciclo while
+"""
+#Solución 1
+contador = 20
+while contador<51:
+    print(contador)
+    contador += 1
+
+#Solución 2
+contador2 = 20
+while True:
+    print(contador2)
+    contador2 += 1
+    if contador2 > 50:
+        break
+
+""" 
+Mostrar en pantalla los primeros 10 numeros de la serie
+de fibonacci
+"""
+(1,1,2,3, 5, 8, 13, 21, 34, 55)
+
+
+print("-----fibonacci----")
+n = 1
+fib1 = 1
+fib2 = 1
+print(fib1)
+print(fib2)
+
+while n<9: # Yo prefiero colocar la condición aquí
+    fibNext = fib1 + fib2
+    print(fibNext)
+    fib1, fib2 = fib2, fibNext
+    n += 1
+    
+
+"""
+Realice un programa que solicite
+una contraseña, si la contraseña es
+incorrecta, solicite la contraseña 
+nuevamente.
+en caso de que si coincida, 
+termine el ciclo
+"""
+
+contraseñaUsuario = input("Ingrese una contraseña: ")
+contraseñaOriginal = "1234"
+
+while contraseñaOriginal != contraseñaUsuario:
+    print("Contraseña incorrecta")
+    contraseñaUsuario = input("Ingrese una contraseña: ")
+    
+print("Contraseña correcta")
+
+
+
+
+
+
 
