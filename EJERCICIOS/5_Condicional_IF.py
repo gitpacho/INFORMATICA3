403a7341eeb022341e38bd93ffb06098ddd12424
gitpacho <crepachonpa@unal.edu.co>
2022-09-07 14:29:24 -0500

    Condicional if teoria

---
+++ EJERCICIOS/5_Condicional_IF.py
@@ -0,0 +1,31 @@
+"""
+Determine si un numero es mayor a 18, 
+utilizando el condicional IF
+"""
+
+numero = 18
+if numero > 18:
+    print("Numero es mayor a 18")
+
+"""
+Determine si un numero es mayor o no a 18, 
+utilizando el condicional IF-ELSE
+"""
+numero = 18
+if numero > 18:
+    print("Numero es mayor a 18")
+else:
+    print("Numero no es mayor a 18")
+
+
+"""
+Pedir a el usuario que ingrese 3 numeros,
+luego, retornar el numero mayor y el menor
+"""
+
+
+
+
+
+
+
