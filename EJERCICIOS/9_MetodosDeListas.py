6b637cd2e9be91c1f8a2a5f0ddfe79639d8d2bd3
gitpacho <crepachonpa@unal.edu.co>
2022-09-30 14:59:17 -0500

    Ejercicios Clase Listas

---
+++ EJERCICIOS/9_MetodosDeListas.py
@@ -0,0 +1,39 @@
+#30-09-2022
+
+
+""" 
+crear la siguientes listas
+
+lista1 = [1,2,3,4,5,6,7,8]
+lista2 = list(range(20)) + ["a", "b", "c", 100]
+lista3 = ["cruel", "mundo", "hola", 1, 100, 200, 500]
+
+Realice lo siguiente:
+
+*Agregar al final de lista1, los elementos => 1,2,3,4
+*Agregar al comienzo de lista1, los elementos => 0,0,2,4
+*Eliminar los 3 ultimos elementos de lista1
+*Eliminar los 2 primeros elementos de lista1
+
+*Sume todos los elementos de lista 2 que pueden operarse algebraicamente
+*Organice lista2 de forma ascendente
+*Elimine todos los elementos de lista2 original que son enteros 
+
+*Sume todos los elementos de lista3 que pueden operarse algebraicamente
+ sin alterar ni cambiar lista3 original
+*Haga una copia de lista3 de la siguiente manera
+ lista3Copia = lista3
+ y agregue 3 nuevos elementos sobre esta nueva lista.
+ ¿qué sucede con la lista3 original?
+*Encuentre una manera de alterar la copia sin afectar la lista original
+
+INDEXADO => Extraer el elemento inicial de lista1  (de dos maneras)
+            Extraer el elemento final de lista1  (de dos maneras)
+            Extraer el elemento del medio de lista2 (de dos maneras)
+SLICING  => Extraer los primeros 3 elementos de lista1, lista2 y lista3
+             y colocarlos en una nuevaLista
+            Extraer cada 2 elementos de lista 2
+            Extraer todos los elementos de lista3 al revés
+            Extraer los elementos de lista3 ubicados en indices impares
+
+"""
