885dadbbc090679f1c38cb0f22d6a285256d3317
gitpacho <crepachonpa@unal.edu.co>
2022-10-05 14:37:42 -0500

    ejercicios Diccionarios

---
+++ EJERCICIOS/10_MetodosDeDiccionarios.py
@@ -0,0 +1,52 @@
+# 05 - 10 - 2022
+
+#---------------------EJERCICIOS DICCIONARIOS ---------------------
+
+#Los diccionarios son una estructura de datos que contienen
+#un par clave-valor. Permite acceder a la información de una manera
+# mas personalizada y eficiente
+
+diccionarioEstudiantes = {
+    "Cristian Pachon": 2.0,
+    "Maria Bermudez" : 3.0,
+    "Camilo Ibarra" : 4.0,
+    "Fernanda Gutierrez": 5.0
+}
+
+diccionarioPaises = {
+    "Colombia": ["Cali", "Manizales", "Cartagena"],
+    "Argentina": ["Buenos Aires", "La plata", "Cordoba"],
+    "Brasil": ["Sao Paulo", "Brasilia", "Minas Gerais"]
+}
+
+diccionarioNumeros = {
+    1: "uno",
+    2: "dos",
+    3: "tres",
+    4: "cuatro",
+    5: "cinco"
+}
+
+# ¿Cómo extraer un valos dada una clave
+"""Extraer del diccionario estudiantes el valor de la clave Camilo Ibarra"""
+
+valor = diccionarioEstudiantes["Camilo Ibarra"]
+print(valor)
+
+
+
+
+
+# ¿Cómo eliminar un par clave-valor?
+# ¿Cómo cambiar un valor?
+# ¿Cómo agregar un par clave-valor?
+# ¿cómo extraer las claves de un diccionario?
+# ¿ Cómo extraer los valores de un diccionario
+# ¿ Como extraer los pares clave-valor?
+
+
+
+
+
+
+
