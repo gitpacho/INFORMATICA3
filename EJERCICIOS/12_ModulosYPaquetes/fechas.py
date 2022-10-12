945bc0b8a01a93365294ea71ab8d63eb17657538
gitpacho <crepachonpa@unal.edu.co>
2022-10-12 15:33:14 -0500

    updated

---
+++ EJERCICIOS/12_ModulosYPaquetes/fechas.py
@@ -0,0 +1,12 @@
+"""
+Este modulo contiene fechas de interes
+para el profe
+"""
+
+fechaImportante1 = "20-07"  # Dia independencia
+fechaImportante2 = "15-05"  # Dia del profe
+fechaImportante3 = "06-03" # Nacimiento Gabriel Garcia Marquez
+fechaImportante4 = "12-08" # Adopte un gatico
+fechaImportante5 = "31-12" # Fin de año
+fechaImportante6 = "31-06" # Mitad de año
+
