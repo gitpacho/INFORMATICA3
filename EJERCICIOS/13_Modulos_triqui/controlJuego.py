d3f5e8200531bab83be25ec5ea02f12ae1e9dfa6
gitpacho <crepachonpa@unal.edu.co>
2022-10-26 14:28:40 -0500

    updates

--- EJERCICIOS/13_Modulos_triqui/controlJuego.py
+++ EJERCICIOS/13_Modulos_triqui/controlJuego.py
@@ -4,4 +4,8 @@ Aqui se controla el juego triqui
 
 import interfazJuego, logicaJuego
 
+interfazJuego.explicarJuego()
+tableroJuego = logicaJuego.generarTableroLogico()
 
+for turno in ["x", "o", "x", "o", "x", "o", "x", "o", "x"]:
+    print(turno)
