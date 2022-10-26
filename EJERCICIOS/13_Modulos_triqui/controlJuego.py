ea62dbdc028872acebcfdf047b684c175ab94e2e
gitpacho <crepachonpa@unal.edu.co>
2022-10-26 17:33:33 -0500

    updates

--- EJERCICIOS/13_Modulos_triqui/controlJuego.py
+++ EJERCICIOS/13_Modulos_triqui/controlJuego.py
@@ -2,22 +2,23 @@
 Aqui se controla el juego triqui
 """
 
-import interfazJuego, logicaJuego
+import interfazJuego
+import logicaJuego
 
 interfazJuego.explicarJuego()
 input(".....Ingrese enter para continuar....")
-
 tableroJuego = logicaJuego.generarTableroLogico()
+
 for turno in ["x", "o", "x", "o", "x", "o", "x", "o", "x"]:
     print("\nTurno judador: ", turno)
     while True:
         posicion = int(input("   Ingrese posicion de juego: "))
         validez = logicaJuego.validarPosicion(tableroJuego, posicion)
         if validez == True: break
-        print("   warning: posicion invalida!")
-    tablero = logicaJuego.insertarCaracterEnTablero(tableroJuego, posicion, turno)
-    interfazJuego.imprimirTablero(tablero)
-    posibleGanador = logicaJuego.determinarGanador(tablero)
+        else: print("   warning: posicion invalida!")
+    tableroJuego = logicaJuego.insertarCaracterEnTablero(tableroJuego, posicion, turno)
+    interfazJuego.imprimirTablero(tableroJuego)
+    posibleGanador = logicaJuego.determinarGanador(tableroJuego)
     if posibleGanador in ["x", "o"]: 
         print("Felicidades {} acaba de ganar juego".format(posibleGanador))
         break
--- EJERCICIOS/14_ModulosPreinstalados/modulos.py
+++ EJERCICIOS/14_ModulosPreinstalados/modulos.py
@@ -1,5 +1,10 @@
-import math
+#IMPORTANTE!
 
+#1)Para ejecutar las siguientes lineas instale primero los modulos (random, math, calendar, matplotlib, numpy, tqdm ...)
+#2)Los modulos se instalan en la terminal con: pip install <modulo> 
+#3)Si los problemas persisten utilice google colab, que no requiere instalar los modulos
+
+import math
 """
 imprimir las variables pi y euler,
 ejecutar al menos una funcion de ese modulo
@@ -11,19 +16,17 @@ print("seno(3.14/2) => ", math.sin(math.pi/2))
 
 
 import random
-
 """
 imprimir 10 numeros aleatorios. opciones: flotante del 0 al 1
-imprimir 10 numero aleatorio. opciones: 1,2,3,4,5
-imprimir 10 caracter aleatorio. opciones: "a", "b", "c"
+imprimir 10 numeros aleatorios. opciones: 1,2,3,4,5
+imprimir 10 caracteres aleatorios. opciones: "a", "b", "c"
 """
 print("numero aleatorio [0,1)")
 for i in range(10):
     numeroAleatorio = random.random()
     print(numeroAleatorio)
 
-
-print("numero aleatorio [1,2,3,4,5]")
+print("numero aleatorio 1,2,3,4,5")
 for i in range(10):
     numeroAleatorio = random.choice([1,2,3,4,5])
     print(numeroAleatorio)
@@ -35,31 +38,26 @@ for i in range(10):
 
 
 import tqdm
-
 """
 Ejecutar un for con una barra de progreso
 """
-#revisar tqdm.trange()
-for i in tqdm.tqdm(range(1000000)):
+for i in tqdm.tqdm(range(10000000)):
     10 * 5
 print("proceso Terminado")
 
 
 import calendar
-
 """
 Imprimir un calendario
 """
-
 print(calendar.calendar(2022))
 
 
 import numpy  #pip install numpy
 import matplotlib.pyplot as plt #pip install matplotlib
-
 """
 Utilizar la estructura basica de numpy: un arreglo
-luego graficar usando matplotlib la funcion x**2 + 1
+luego graficar usando matplotlib (arreglo vs arreglo**2)
 """
 
 arreglo1 = numpy.array([1,2,3,4,5])
@@ -69,5 +67,3 @@ arreglo3 = arreglo2 ** 2
 plt.figure()
 plt.plot(arreglo2, arreglo3)
 plt.show()
-
-
