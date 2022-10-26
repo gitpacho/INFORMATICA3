cdea33e24ac47e714568cbb75c354e4c8c872f82
gitpacho <crepachonpa@unal.edu.co>
2022-10-26 15:30:38 -0500

    updates

--- EJERCICIOS/13_ModulosPreinstalados/modulos.py
+++ EJERCICIOS/13_ModulosPreinstalados/modulos.py
@@ -14,11 +14,18 @@ import random
 
 """
 imprimir 10 numeros aleatorios. opciones: flotante del 0 al 1
-imprimir un numero aleatorio. opciones: 1,2,3,4,5
-imprimir un caracter aleatorio. opciones: "a", "b", "c"
+imprimir 10 numero aleatorio. opciones: 1,2,3,4,5
+imprimir 10 caracter aleatorio. opciones: "a", "b", "c"
 """
 print("numero aleatorio [0,1)")
 for i in range(10):
     numeroAleatorio = random.random()
     print(numeroAleatorio)
+
+
+print("numero aleatorio [1,2,3,4,5]")
+for i in range(10):
+    numeroAleatorio = random.choice([1,2,3,4,5])
+    print(numeroAleatorio)
+
 
