a83a38b7542b30b2cc5e930c0190dd1cd19b2725
gitpacho <crepachonpa@unal.edu.co>
2022-10-26 15:25:09 -0500

    modulos integrados

---
+++ EJERCICIOS/13_ModulosPreinstalados/modulos.py
@@ -0,0 +1,21 @@
+import math
+
+"""
+imprimir las variables pi y euler,
+ejecutar al menos una funcion de ese modulo
+"""
+
+print("pi => ", math.pi)
+print("euler => ", math.e)
+print("seno(3.14/2) => ", math.sin(math.pi/2))
+
+
+import random
+
+"""
+imprimir 10 numeros aleatorios. opciones: flotante del 0 al 1
+imprimir un numero aleatorio. opciones: 1,2,3,4,5
+imprimir un caracter aleatorio. opciones: "a", "b", "c"
+"""
+numeroAleatorio = random.random()
+print(numeroAleatorio)
