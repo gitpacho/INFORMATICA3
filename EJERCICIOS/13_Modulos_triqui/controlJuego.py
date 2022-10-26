ce2c5628e3caee238bd0abd66a98c64f327a70d2
gitpacho <crepachonpa@unal.edu.co>
2022-10-26 14:44:42 -0500

    update

--- EJERCICIOS/13_Modulos_triqui/controlJuego.py
+++ EJERCICIOS/13_Modulos_triqui/controlJuego.py
@@ -9,7 +9,7 @@ input(".....Ingrese enter para continuar....")
 
 tableroJuego = logicaJuego.generarTableroLogico()
 for turno in ["x", "o", "x", "o", "x", "o", "x", "o", "x"]:
-    print("Turno judador: ", turno)
+    print("\nTurno judador: ", turno)
     posicion = int(input("   Ingrese posicion de juego: "))
     tablero = logicaJuego.insertarCaracterEnTablero(tableroJuego, posicion, turno)
     interfazJuego.imprimirTablero(tablero)
