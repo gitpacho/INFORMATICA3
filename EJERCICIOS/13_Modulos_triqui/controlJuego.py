579799fd446444f57f191e127bd0837322267f90
gitpacho <crepachonpa@unal.edu.co>
2022-10-26 14:56:24 -0500

    updates

--- EJERCICIOS/13_Modulos_triqui/controlJuego.py
+++ EJERCICIOS/13_Modulos_triqui/controlJuego.py
@@ -12,8 +12,10 @@ for turno in ["x", "o", "x", "o", "x", "o", "x", "o", "x"]:
     print("\nTurno judador: ", turno)
     while True:
         posicion = int(input("   Ingrese posicion de juego: "))
-        validez = logicaJuego.validarPosicion
-        if validez == True: break
+        validez = logicaJuego.validarPosicion(tableroJuego, posicion)
+        if validez == True: 
+            print("   warning: posicion invalida!")
+            break
     tablero = logicaJuego.insertarCaracterEnTablero(tableroJuego, posicion, turno)
     interfazJuego.imprimirTablero(tablero)
     posibleGanador = logicaJuego.determinarGanador(tablero)
