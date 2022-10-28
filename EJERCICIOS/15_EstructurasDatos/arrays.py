d0e891854ac2c34f02e18de4c8bcd950ce311cb6
gitpacho <crepachonpa@unal.edu.co>
2022-10-28 14:59:42 -0500

    arrays, series, dataframes

---
+++ EJERCICIOS/15_EstructurasDatos/arrays.py
@@ -0,0 +1,32 @@
+"""
+Ejemplos: crear los siguientes vectores, matrices y tensores
+
+
+vector1 = 1,2,3,4,5,6,100
+vector2 = 10,20,30,40,50
+
+matriz1 = [1 1 1
+           1 1 1
+           1 1 1]
+matriz2 = [1 2 3
+           4 5 6
+           7 8 9]
+
+tensor1=[[0 0 0
+          0 0 0
+          0 0 0]
+         [2 2 2
+          2 2 2
+          2 2 2]] 
+
+tensor2= [[1  2  3
+           4  5  6
+           7  8  9]
+          [10 11 12
+           13 14 15
+           16 17 18]] 
+
+
+
+
+"""
