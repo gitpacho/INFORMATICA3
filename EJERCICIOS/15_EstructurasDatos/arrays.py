adc5c9965e22a4cc6caa9fe6ad4a7237c4d39816
gitpacho <crepachonpa@unal.edu.co>
2022-10-28 15:43:49 -0500

    stacking

--- EJERCICIOS/15_EstructurasDatos/arrays.py
+++ EJERCICIOS/15_EstructurasDatos/arrays.py
@@ -2,7 +2,7 @@
 Ejemplos: crear los siguientes vectores, matrices y tensores
 
 
-vector1 = 1,2,3,4,5,6,100
+vector1 = 1,2,3,4,5
 vector2 = 10,20,30,40,50
 
 matriz1 = [1 1 1
@@ -29,7 +29,7 @@ tensor2= [[1  2  3
 import numpy as np
 
 #Como crear los vectores numpy
-vector1 = np.array([1,2,3,4,5,6,100])
+vector1 = np.array([1,2,3,4,5])
 vector2 = np.array([10,20,30,40,50])
 
 #como crear matrices numpy
@@ -65,16 +65,53 @@ tensor2= np.array([ [[1,  2,  3],
 ejemplos: como crear los anteriores elementos de manera rapida
 """
 
-vector1 = np.arange(1,7,1) #inicio,fin,salto
+vector1 = np.arange(1,6,1) #inicio,fin,salto
 vector2 = np.arange(10, 51, 10)
 
 matriz1 = np.ones((3,3))   #tamaño (filas,columnas)
 matriz2 = np.arange(1, 10).reshape((3,3))  #reshape (filas, columnas)
 
-tensor1 = 
-tensor2 = 
-print(matriz2)
+tensor1 = "pendiente"
+tensor2 = np.arange(1,19).reshape((2,3,3))
 
 
 
-#reshape
+"""
+ejemplos: a) como apilar vector1 y vector2 y formar vector mas grande
+          b) como apilar vector1 y vector2 y formar una matriz
+          c) como apilar matriz1 y matriz1 y formar matriz mas grande
+          d) como apilar matriz1 y matriz1 y formar un tensor
+"""
+
+"""
+vector1 = 1,2,3,4,5,6,100
+vector2 = 10,20,30,40,50
+vectorResultante = 1,2,3,4,5,6,100, 10,20,30,40,50 ?????
+"""
+vectorResultante = np.hstack((vector1, vector2))
+print(vectorResultante)
+
+"""
+vector1 = 1,2,3,4,5,6,100
+vector2 = 10,20,30,40,50
+matrizResultante = 1,2,3,4,5,6,100, 
+                   10,20,30,40,50
+"""
+
+matrizResultante = np.vstack((vector1, vector2))
+print(matrizResultante)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
