f1caaa2d1c6f6f998f87fd444063f194e1bb2ea0
gitpacho <crepachonpa@unal.edu.co>
2022-10-28 14:43:10 -0500

    EJERCICIOS MODULOS

--- EJERCICIOS/EJERCICIOS_4.py
+++ EJERCICIOS/EJERCICIOS_4.py
@@ -1,10 +1,26 @@
 """ En este archivo encontramos ejercicios con los siguientes temas: 
 
-        * CLASES Y OBJETOS
+        * Modulos
         * 
         * 
 
 """
+
+#====================== EJERCICIOS MODULOS ====================
+
+"""
+Cree los modulos que indica cada ejercicio, para desarrollar los siguientes programas:
+
+1) Algoritmo conversor de numeros     => modulos: decimal binario hexadecimal octal, control
+2) Algoritmo para jugar un sudoku 4*4 => modulos: logica, interfaz, control
+3) Herramienta de calificaciones      => modulos: estudiante, materia, control
+4) Algoritmo operador de vectores     => modulos: vector, operacionesVectores, control
+5) Algoritmo operador de matrices     => modulos: matriz, operacionesMatrices, control 
+
+Además, tenga en cuenta agregar la documentacion de cada modulo con las funciones que considere necesarias,
+Recuerde que el modulo control, consume los demás modulos y organiza el respectivo programa
+"""
+
 
 #====================== EJERCICIOS CLASES Y OBJETOS ====================
 
