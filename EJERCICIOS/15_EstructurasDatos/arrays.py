fe34aae3a71e34b14b055556d922eb5c1c98830c
gitpacho <crepachonpa@unal.edu.co>
2022-10-28 23:35:55 -0500

    arreglos numpy

--- EJERCICIOS/15_EstructurasDatos/arrays.py
+++ EJERCICIOS/15_EstructurasDatos/arrays.py
@@ -1,38 +1,38 @@
+#####################################################################
+""" Ejemplo: Crear los siguientes vectores, matrices y tensores. (Para ello utilice arreglos de numpy)
+
+                     => vector1 = 1,2,3,4,5
+                     => vector2 = 10,20,30,40,50
+
+                     => matriz1 = [1 1 1
+                                1 1 1
+                                1 1 1]
+                     => matriz2 = [1 2 3
+                                4 5 6
+                                7 8 9]
+
+                     => tensor1=[[0 0 0
+                                0 0 0
+                                0 0 0]
+                                [2 2 2
+                                2 2 2
+                                2 2 2]] 
+
+                     => tensor2= [[1  2  3
+                                4  5  6
+                                7  8  9]
+                                [10 11 12
+                                13 14 15
+                                16 17 18]] 
 """
-Ejemplos: crear los siguientes vectores, matrices y tensores
 
-
-vector1 = 1,2,3,4,5
-vector2 = 10,20,30,40,50
-
-matriz1 = [1 1 1
-           1 1 1
-           1 1 1]
-matriz2 = [1 2 3
-           4 5 6
-           7 8 9]
-
-tensor1=[[0 0 0
-          0 0 0
-          0 0 0]
-         [2 2 2
-          2 2 2
-          2 2 2]] 
-
-tensor2= [[1  2  3
-           4  5  6
-           7  8  9]
-          [10 11 12
-           13 14 15
-           16 17 18]] 
-"""
 import numpy as np
 
-#Como crear los vectores numpy
+# Vectores con numpy
 vector1 = np.array([1,2,3,4,5])
 vector2 = np.array([10,20,30,40,50])
 
-#como crear matrices numpy
+# Matrices con numpy
 matriz1 = np.array([[1, 1, 1],
                     [1, 1, 1],
                     [1, 1, 1]])
@@ -41,7 +41,7 @@ matriz2 = np.array([[1, 2, 3],
                     [4, 5, 6],
                     [7, 8, 9]])
 
-#como crear tensores
+# Tensores con numpy
 tensor1= np.array([ [[0, 0, 0],
                      [0, 0, 0],
                      [0, 0, 0]],
@@ -50,7 +50,6 @@ tensor1= np.array([ [[0, 0, 0],
                      [2, 2, 2],
                      [2, 2, 2]]
                  ])
-
 tensor2= np.array([ [[1,  2,  3],
                      [4,  5,  6],
                      [7,  8,  9]],
@@ -61,91 +60,108 @@ tensor2= np.array([ [[1,  2,  3],
                  ])
 
 
+#####################################################################
 """
-ejemplos: como crear los anteriores elementos de manera rapida
+Ejemplo: ¿Cómo crear los anteriores arreglos (vectores, matrices, tensores) de manera automática?
 """
-
 vector1 = np.arange(1,6,1) #inicio,fin,salto
 vector2 = np.arange(10, 51, 10)
 
 matriz1 = np.ones((3,3))   #tamaño (filas,columnas)
 matriz2 = np.arange(1, 10).reshape((3,3))  #reshape (filas, columnas)
 
-tensor1 = "pendiente"
+tensor1 = "pendiente!!!"
 tensor2 = np.arange(1,19).reshape((2,3,3))
 
 
 
 """
-ejemplos: a) como apilar vector1 y vector2 y formar vector mas grande
-          b) como apilar vector1 y vector2 y formar una matriz
-          c) como apilar matriz1 y matriz2 y formar matriz mas ancha
-          d) como apilar matriz1 y matriz1 y formar matriz más larga
+ejemplos: a) ¿Cómo apilar vector1 y vector2 ? (formar nuevo vector mas largo)
+          b) ¿Cómo apilar vector1 y vector2 ? (formar nueva matriz)
+          c) ¿Cómo apilar matriz1 y matriz2 ? (formar nueva matriz mas ancha)
+          d) ¿Cómo apilar matriz1 y matriz2 ? (formar nueva matriz más larga)
+          e) ¿Cómo apilar un vector y una matriz? (formar nueva matriz más larga o más ancha)
 """
 
-"""
-vector1 = 1,2,3,4,5,6,100
-vector2 = 10,20,30,40,50
-vectorResultante = 1,2,3,4,5,6,100, 10,20,30,40,50 ?????
+""" a) ¿Cómo apilar vector1 y vector2 ? (formar nuevo vector mas largo)
+        
+        vector1 = 1 2 3 4 5       vector2 = 10 20 30 40 50
+        
+        vectorResultante = 1 2 3 4 5 10 20 30 40 50 
 """
 vectorResultante = np.hstack((vector1, vector2))
 print(vectorResultante)
 
-"""
-vector1 = 1,2,3,4,5,6,100
-vector2 = 10,20,30,40,50
-matrizResultante = 1,2,3,4,5,6,100, 
-                   10,20,30,40,50
+
+""" b) ¿Cómo apilar vector1 y vector2 ? (formar nueva matriz)
+        
+        vector1 = 1 2 3 4 5     vector2 = 10 20 30 40 50
+
+        matrizResultante =   1  2  3  4  5
+                            10 20 30 40 50
 """
 
 matrizResultante = np.vstack((vector1, vector2))
 print(matrizResultante)
 
 
-"""
-matriz1 = [1 1 1
-           1 1 1
-           1 1 1]
-matriz2 = [1 2 3
-           4 5 6
-           7 8 9]
-
-* Apilarlas horizontalmente
-matrizAncha  = [1 1 1 1 2 3
-                1 1 1 4 5 6
-                1 1 1 7 8 9]
-
-* Apilarlas verticalmente
-matrizLarga = [1 1 1 
-               1 1 1 
-               1 1 1 
-               1 2 3
-               4 5 6
-               7 8 9]
+""" c) ¿Cómo apilar matriz1 y matriz2 ? (formar nueva matriz mas ancha)
+        
+        matriz1 =  1 1 1     matriz2 = 1 2 3
+                   1 1 1               4 5 6
+                   1 1 1               7 8 9
+        
+        => Apilarlas horizontalmente
+        matrizAncha  =  1 1 1 1 2 3
+                        1 1 1 4 5 6
+                        1 1 1 7 8 9
 """
 
 matrizAncha = np.hstack((matriz1, matriz2))
-matrizLarga = np.vstack((matriz1, matriz2))
 print(matrizAncha)
+
+
+""" d) ¿Cómo apilar matriz1 y matriz2 ? (formar nueva matriz más larga)
+
+        matriz1 =  1 1 1     matriz2 =  1 2 3
+                   1 1 1                4 5 6
+                   1 1 1                7 8 9
+    
+        => Apilarlas verticalmente
+        matrizLarga =  1 1 1 
+                       1 1 1 
+                       1 1 1 
+                       1 2 3
+                       4 5 6
+                       7 8 9
+"""
+
+matrizLarga = np.vstack((matriz1, matriz2))
 print(matrizLarga)
 
 
 """
-como apilar un vector con una matriz de forma 
-horizontal y vertical
+        e) ¿Cómo apilar una matriz y un vector? (formar una matriz más larga o más ancha)
+
+        matriz =  1 2 3      vector = 10 20 30
+                  4 5 6                            
+                  7 8 9                            
+
+        matrizResultante1 =  1  2  3
+                             4  5  6
+                             7  8  9
+                             10 20 30
+
+        matrizResultante2 =  1  2  3 10
+                             4  5  6 20
+                             7  8  9 30
+           
 """
-vectorNuevo = np.array([1,2,3])
-matrizNueva = np.arange(1,10).reshape((3,3))
-print("=>\n", vectorNuevo)
-print("=>\n", matrizNueva)
+vector = np.array([10,20,30])
+matriz = np.arange(1,10).reshape((3,3))
 
-matrizNueva1 = np.vstack((matrizNueva, vectorNuevo))
-print(matrizNueva1)
+matrizResultante1 = np.vstack((matriz, vector))
+print("=>", matrizResultante1)
 
-matrizNueva1 = np.hstack((matrizNueva, vectorNuevo.reshape((3,1))))
-print(matrizNueva1)
-
-
-
-
-
+matrizResultante2 = np.hstack((matriz, vector.reshape((3,1))))
+print("=>", matrizResultante2)
