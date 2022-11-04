4c0cd20811f330b264c0da3943307c6d1ba2b549
gitpacho <crepachonpa@unal.edu.co>
2022-11-04 14:40:42 -0500

    ejercicios series

--- EJERCICIOS/15_EstructurasDatos/series.py
+++ EJERCICIOS/15_EstructurasDatos/series.py
@@ -21,5 +21,12 @@ Crear las siguientes series
     Mauricio Velazquez   5.0 
     Esteban Rodriguez    3.2 
 
- * Ventas
+ * Rendimiento deportivo
+   
+   Cod. Deportista (str) =>   001   002   003   004   005   006   007   008   009    010   011   012    013    014    015    016    017   018  019  020  021  022   023   024   025  026  027   028   029   030   031   032   033    034    035   036    037   038   039   040 041   042   043   044   045  046   047   048   049   050   051  052    053    054   055   056   057   058   059  060  061   062   063   064   065   066  067   068   069   070   071   072   073    074   075   076    077   078   079   080   081   082   083   084   085  086   087   088   089   090   091  092    093    094   095    096   097  098  099  100
+   rendimiento     (str) =>   A     B     C     B     B     B     C     B     A      C     B     A      C      B      B      B      B     A    B    A    A    C     B     B     B    B    C     B     B     C     B     A     C      B      A     C      B     B     B     C   B     C     B     A     C     B    A     C     B     B     B    B      A      B     A     A     C     B     B    B    B     C     B     B     C     B    A     C     B     A     C     B     B      B     C     A      B     C     B     B     A     B     C     B     B    B     B     B     A     B     B    A      C      B     B      B     B    A    B    B  
+
+
+
+
 """
