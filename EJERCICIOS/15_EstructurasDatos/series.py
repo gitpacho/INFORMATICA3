08c9f715796f61f6306ebf7549935d25c04fba3c
gitpacho <crepachonpa@unal.edu.co>
2022-11-04 14:38:21 -0500

    series

--- EJERCICIOS/15_EstructurasDatos/series.py
+++ EJERCICIOS/15_EstructurasDatos/series.py
@@ -6,5 +6,20 @@ Crear las siguientes series
     cantidades  22  3   21  10  15  18  14  13  22  12  98  32  51  45  60
 
 * Calificaciones
+    Nombre              Nota 
+    Miguel Pineda        1.0 
+    Maria Gonzalez       3.1 
+    Jose Nuñez           5.0 
+    Angelica Lozano      3.1 
+    Camilo Suarez        3.2 
+    Mariana Rosero       5.0 
+    Esteban Quesada      3.4 
+    Julia Quintero       2.0 
+    Mauricio Lizcano     3.7 
+    Angie Gomez          4.1 
+    Camilo Restrepo      5.0 
+    Mauricio Velazquez   5.0 
+    Esteban Rodriguez    3.2 
 
+ * Ventas
 """
