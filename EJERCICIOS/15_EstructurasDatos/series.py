29ffde5599bf2d76cc6159b8d082b0162e9c1bc5
gitpacho <crepachonpa@unal.edu.co>
2022-11-04 14:36:02 -0500

    Series ejercicios

---
+++ EJERCICIOS/15_EstructurasDatos/series.py
@@ -0,0 +1,10 @@
+"""
+Crear las siguientes series
+
+* Ventas de productos
+    codigo      001 002 003 004 005 006 007 008 009 010 011 012 013 014 015
+    cantidades  22  3   21  10  15  18  14  13  22  12  98  32  51  45  60
+
+* Calificaciones
+
+"""
