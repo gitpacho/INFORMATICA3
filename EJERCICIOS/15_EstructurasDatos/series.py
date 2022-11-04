26c7ab5ef34bf186ee2e667737a66d75cac74754
gitpacho <crepachonpa@unal.edu.co>
2022-11-04 16:11:22 -0500

    series exercises

--- EJERCICIOS/15_EstructurasDatos/series.py
+++ EJERCICIOS/15_EstructurasDatos/series.py
@@ -35,18 +35,15 @@ data =np.array([22, 3,  21, 10, 15, 18, 14, 13, 22, 12, 98, 32, 51, 45, 60])
 serieVentas = pd.Series(data = data, index=index)
 print(serieVentas)
 
-
 index = ["Miguel Pineda","Maria Gonzalez","Jose Nuñez","Angelica Lozano","Camilo Suarez","Mariana Rosero","Esteban Quesada","Julia Quintero","Mauricio Lizcano","Angie Gomez","Camilo Restrepo","Mauricio Velazquez","Esteban Rodriguez"]
 data = np.array([1.0,3.1,5.0,3.1,3.2,5.0,3.4,2.0,3.7,4.1,5.0,5.0,3.2])
 serieCalificaciones = pd.Series(data=data, index=index)
 print(serieCalificaciones)
 
-
 index =  ["001",   "002",   "003",   "004",   "005",   "006",   "007",   "008",   "009",    "010",   "011",   "012",    "013",    "014",    "015",    "016",    "017",   "018",  "019",  "020",  "021",  "022",   "023",   "024",   "025",  "026",  "027",   "028",   "029",   "030",   "031",   "032",   "033",    "034",    "035",   "036",    "037",   "038",   "039",   "040", "041",   "042",   "043",   "044",   "045",  "046",   "047",   "048",   "049",   "050",   "051",  "052",    "053",    "054",   "055",   "056",   "057",   "058",   "059",  "060",  "061",   "062",   "063",   "064",   "065",   "066",  "067",   "068",   "069",   "070",   "071",   "072",   "073",    "074",   "075",   "076",    "077",   "078",   "079",   "080",   "081",   "082",   "083",   "084",   "085",  "086",   "087",   "088",   "089",   "090",   "091",  "092",    "093",    "094",   "095",    "096",   "097",  "098",  "099",  "100"]
 data =   np.array(["A","B","C","B","B","B","C","B","A","C","B","A", "C",  "B",  "B",  "B",  "B", "A","B","A","A","C", "B", "B", "B","B","C", "B", "B", "C", "B", "A", "C",  "B",  "A", "C",  "B", "B", "B", "C",   "B", "C", "B", "A", "C", "B","A", "C", "B", "B", "B","B",  "A",  "B", "A", "A", "C", "B", "B","B","B", "C", "B", "B", "C", "B","A", "C", "B", "A", "C", "B", "B",  "B", "C", "A",  "B", "C", "B", "B", "A", "B", "C", "B", "B","B", "B", "B", "A", "B",  "B", "A","C","B",  "B","B",  "B", "A", "B", "B"])
 serieDeportistas = pd.Series(data=data, index=index)
 print(serieDeportistas)
-
 
 
 """
@@ -59,17 +56,14 @@ print("data =>", serieVentas.values)
 print("tamaño =>", serieVentas.shape)
 
 
-
 """
 Imprimir la media, desviacion, el minimo y el maximo de la serieCalificaciones
 """
-
 
 media = serieCalificaciones.mean()
 desviacion = serieCalificaciones.std()
 min = serieCalificaciones.min()
 max = serieCalificaciones.max()
-
 print("estadisticos=>", media, desviacion, min, max)
 
 
@@ -79,7 +73,6 @@ Imprimir el codigo del producto menos vendido, y el codigo del producto mas vend
 
 productoMenosVendido = serieVentas.idxmin()
 productoMasVendido = serieVentas.idxmax()
-
 print("productos => ", productoMenosVendido, productoMasVendido)
 
 
@@ -122,9 +115,5 @@ b) Calcular de serie serieVentasProductos lo siguiente:
                 *frecuencias
 c) Determine cual es el producto mas costoso y el menos costoso 
 
-c) Calcular el dinero recaudado por producto 
+d) Calcular el dinero recaudado por producto 
 """
-
-
-
-
