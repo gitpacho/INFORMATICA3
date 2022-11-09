d87d478bc836455b00f1f97cfb7f9d548e0af06f
gitpacho <crepachonpa@unal.edu.co>
2022-11-09 14:58:00 -0500

    dataFrames Ejercicios

--- EJERCICIOS/15_EstructurasDatos/dataFrames.py
+++ EJERCICIOS/15_EstructurasDatos/dataFrames.py
@@ -13,7 +13,7 @@ codigo   tamaño   precio
 import numpy as np
 import pandas as pd
 
-data = np.Array([[1, 12000],   
+data = np.array([[1, 12000],   
                  [2, 15000], 
                  [3, 21000],
                  [2, 32000],
@@ -23,6 +23,32 @@ data = np.Array([[1, 12000],
 hoja1 = pd.DataFrame( data = data,
                       columns = ["tamaño", "precio"],
                       index = ["001","002","003","004","005","006"])
-
 print(hoja1)
 
+
+
+
+"""
+Crear un DataFrame que cumpla con lo siguiente
+
+cada columna va a ser referencia a 5 funciones: ["f1","f2","f3","f4","f5"]
+los indices van a ser la variable independiente: [0, 0.5, 1, 1.5, ...200]
+
+f1(x) = 3x + 5
+f2(x) = x + random
+f3(x) = sen(x)
+f4(x) = e^x
+f5(x) = x*sinx + random
+
+
+    |  f1     f2      f3     f4     f5
+----------------------------------------
+0   |  5    | 0.3  |   0  |  2.71 |  0
+0.5 |  6.5  | 0.7  |   .  |   .   |  .
+1   |  8    | 1.2  |   .  |   .   |  .
+1.5 |  9.5  | 1.6  |   .  |   .   |  .
+.   |  .    | .    |   .  |   .   |  .
+.   |  .    | .    |   .  |   .   |  .
+.   |  .    | .    |   .  |   .   |  .
+200 |  605  | 200.2|   .  |   .   |  .
+"""
