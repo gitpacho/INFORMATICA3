d53ede18c33443e0277c3970856bd8061c1c95dc
gitpacho <crepachonpa@unal.edu.co>
2022-11-11 15:55:31 -0500

    update

--- EJERCICIOS/16_ManipulacionDeDatos/Derivadas_E_Integrales.py
+++ EJERCICIOS/16_ManipulacionDeDatos/Derivadas_E_Integrales.py
@@ -4,13 +4,13 @@ import numpy as np
 """
 Utilizamos el data frame del archivo anterior
  """
-x = np.arange(0.01, 1.01, 0.01)
-columna1 = x * np.sin(x) + 0.2 * np.random.rand(100)
-columna2 = np.cos(x) + 0.1 * np.random.rand(100)
+x = np.arange(0, 6.28, 0.01)
+columna1 = x * np.sin(x) + 0.2 * np.random.rand(628)
+columna2 = np.cos(x) + 0.1 * np.random.rand(628)
 columna3 = x * 1/2
-data = np.hstack((columna1.reshape(100,1), columna2.reshape(100,1), columna3.reshape(100,1)))
+data = np.hstack((columna1.reshape(628,1), columna2.reshape(628,1), columna3.reshape(628,1)))
 hoja1 = pd.DataFrame(data= data,
-                     index= np.arange(0.01, 1.01, 0.01),
+                     index= np.arange(0, 6.28, 0.01),
                      columns= ["F1", "F2", "F3"])
 
 
