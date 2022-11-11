1c05d25b552e3980460acb677ff364d6f249be4b
gitpacho <crepachonpa@unal.edu.co>
2022-11-11 15:47:47 -0500

    graficas

--- EJERCICIOS/16_ManipulacionDeDatos/Derivadas_E_Integrales.py
+++ EJERCICIOS/16_ManipulacionDeDatos/Derivadas_E_Integrales.py
@@ -15,15 +15,41 @@ hoja1 = pd.DataFrame(data= data,
 
 
 #------- Metodo numerico para calcular derivadas---------------
+""" 
+ Para calcular derivadas se hace uso de su significado geometrico
+ Una derivada es una pendiente de una funcion
+    f'(x) = (y2-y1) / (x2 -x1)
+ """
 
-derivada1 = np.diff(columna1)/np.diff(x)
-derivada2 = np.diff(columna2)/np.diff(x)
-derivada3 = np.diff(columna3)/np.diff(x)
+derivada1 = np.diff(columna1)/np.diff(x)  #Pendientes de f1
+derivada2 = np.diff(columna2)/np.diff(x)  #Pendientes de f2
+derivada3 = np.diff(columna3)/np.diff(x)  #Pendientes de f2
 
 """
-1) Crear una funcion generica para graficar las derivadas
+1) Crear una funcion genérica para graficar las derivadas
 2) Crear un dataFrame que contenga los valores de las 3 derivadas en 
                             columns = ["der1", "der2", "der3"]
 """
+
+
+import matplotlib.pyplot as plt
+
+def graficaGenerica(x, y, y_prima):
+    plt.figure(figsize=(8,4))
+    plt.plot(x,y, "b", label="f(x)")
+    plt.plot(x[:-1],y_prima, "r", label="f'(x)")
+    plt.xlabel(x)
+    plt.show()
+
+x = hoja1.index
+y = hoja1["F1"]
+y_prima = derivada1
+
+
+graficaGenerica(x, y, y_prima)
+
+
+
+
 
 
