a12400d5bb148abbada2e250d5e3b6a47146cb80
gitpacho <crepachonpa@unal.edu.co>
2022-11-11 16:46:23 -0500

    Manipulacion y visualizacion de datos

--- EJERCICIOS/16_ManipulacionDeDatos/2_derivadas.py
+++ EJERCICIOS/16_ManipulacionDeDatos/2_derivadas.py
@@ -3,24 +3,24 @@
 import pandas as pd
 import numpy as np
 
-"""
-Utilizamos un data frame parecido al del archivo anterior
- """
+"""Creamos el siguiente DataFrame (hoja1)"""
+
 x = np.arange(0, 6.28, 0.01)
 columna1 = x * np.sin(x) + 0.005 * np.random.rand(628)
 columna2 = np.cos(x) + 0.005 * np.random.rand(628)
 columna3 = x * 1/2
 data = np.hstack((columna1.reshape(628,1), columna2.reshape(628,1), columna3.reshape(628,1)))
 hoja1 = pd.DataFrame(data= data,
-                     index= np.arange(0, 6.28, 0.01),
-                     columns= ["F1", "F2", "F3"])
+                     index= x,
+                     columns= ["f1", "f2", "f3"])
 
 
-#------- Metodo numerico para calcular derivadas---------------
 """ 
  Para calcular derivadas se hace uso de su significado geometrico
  Una derivada es una pendiente de una funcion
     f'(x) = (y2-y1) / (x2 -x1)
+
+ Ejemplo: calcular las derivadas de f1, f2 y f3
  """
 
 derivada1 = np.diff(columna1)/np.diff(x)  #Pendientes de f1
@@ -46,17 +46,17 @@ def graficaGenerica(x, y, y_prima, titulo):
     plt.show()
 
 x = hoja1.index
-y = hoja1["F1"]
+y = hoja1["f1"]
 y_prima = derivada1
 graficaGenerica(x, y, y_prima, "f(x) = xsen(x)")
 
 x = hoja1.index
-y = hoja1["F2"]
+y = hoja1["f2"]
 y_prima = derivada2
 graficaGenerica(x, y, y_prima, "f(x) = cos(x)")
 
 x = hoja1.index
-y = hoja1["F3"]
+y = hoja1["f3"]
 y_prima = derivada3
 graficaGenerica(x, y, y_prima, "f(x) = x + 1/2")
 
