493bb7896497e9fbb0109396a0729b01a1e0d697
gitpacho <crepachonpa@unal.edu.co>
2022-11-16 14:41:35 -0500

    integrales numericas

--- EJERCICIOS/16_ManipulacionDeDatos/3_Integrales.py
+++ EJERCICIOS/16_ManipulacionDeDatos/3_Integrales.py
@@ -16,15 +16,36 @@ def graficaGenerica(x,y):
     plt.plot(x,y,"o")
     plt.show()
 
-x = np.arange(0,5.5, 0.5)
+limite_inferior = 0
+limite_superior = 5
+salto = 0.001
+x = np.arange(limite_inferior, limite_superior + salto, salto)
 y = 2 * (x ** 2)
-
 
 integral_numerica = np.trapz(y,x)
 print("teorico = 83.33333333333333")
 print("numerico=", integral_numerica)
 
 graficaGenerica(x,y)
+
+
+
+"""
+calcular las integrales de f1, f2 y f3 del dataFrame hoja1
+"""
+x = np.arange(0, 6.28, 0.01)
+columna1 = x * np.sin(x) + 0.005 * np.random.rand(628)
+columna2 = np.cos(x) + 0.005 * np.random.rand(628)
+columna3 = x * 1/2
+data = np.hstack((columna1.reshape(628,1), columna2.reshape(628,1), columna3.reshape(628,1)))
+hoja1 = pd.DataFrame(data= data,
+                     index= x,
+                     columns= ["f1", "f2", "f3"])
+
+
+
+
+
 
 
 
